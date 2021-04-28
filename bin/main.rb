db43d02f193832871da1932fea47b43d0e9fc176
ZeenatLawal <zeenatlawal82@gmail.com>
2021-04-28 10:14:28 +0100

    add prompt for player's name

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,9 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+puts 'Welcome to Ruby\'s Tic Tac Toe!'
+
+puts 'Enter Player 1 name: '
+player1 = gets.chomp
+
+puts 'Enter Player 2 name: '
+player2 = gets.chomp
+
