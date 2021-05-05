120a7041bcc4299ba35772e4989d194db2bdef84
blakbox23 <boudii30@gmail.com>
2021-05-05 11:16:49 +0300

    Remove linter errors

--- spec/draw_spec.rb
+++ spec/draw_spec.rb
@@ -2,9 +2,9 @@ require './lib/draw'
 
 describe Draw do
   describe '#draw?' do
-    let(:draw){Draw.new(%w[X X O X O O X O X])}
-    let(:not_draw){Draw.new(['X', 'X', 'O', 4, 5, 6, 7, 8, 9])}
-    
+    let(:draw) { Draw.new(%w[X X O X O O X O X]) }
+    let(:not_draw) { Draw.new(['X', 'X', 'O', 4, 5, 6, 7, 8, 9]) }
+
     it 'returns true if board contains strings' do
       expect(draw.draw?).to be true
     end
--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -2,14 +2,14 @@ require './lib/logic'
 
 describe Logic do
   describe '#winner?' do
-    let (:win) {Logic.new([1, 'X', 3, 4, 'X', 6, 7, 'X', 9])}
-    let (:not_win) {Logic.new([1, 'X', 3, 4, 'X', 6, 'X', 'O', 'O'])}
-    
-    it 'returns true if there is a winner' do 
+    let(:win) { Logic.new([1, 'X', 3, 4, 'X', 6, 7, 'X', 9]) }
+    let(:not_win) { Logic.new([1, 'X', 3, 4, 'X', 6, 'X', 'O', 'O']) }
+
+    it 'returns true if there is a winner' do
       expect(win.winner?('X')).to be true
     end
 
-    it 'returns false if there is no winner' do 
+    it 'returns false if there is no winner' do
       expect(not_win.winner?('O')).to be false
     end
   end
